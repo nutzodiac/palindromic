ed0a9c91f683baf3afe25108e542eac98a820400
Nutnicha Neungsawat <Nutnicha_Neu@freewillgroup.com>
2022-04-28 10:40:55 +0700

    add function longest palindrome

--- controllers/palindromicFunction.js
+++ controllers/palindromicFunction.js
@@ -1,3 +1,5 @@
+const { json } = require("express/lib/response");
+
 function reverse(str) {
 	const strLower = str.toLowerCase().replace(/\s/g, '').replace(/[^a-zA-Z 0-9]/gi, "");
   let o = [];
@@ -13,19 +15,26 @@ return word === reverse(word) && !words.includes(word)
 function getStringPalindrome(words) {
   let subStrings = [];
   let len = words.length;
-  let output = [];
+  let palinStringAll = [];
+  let palinStringLongest = "", longestWord = 0;
   for (let i = 0; i < len; i++) {
 	   for(let j = 0; j < len - i; j++) {
 		  let subString = words.substring(j, j + i + 1);
 		  if(isPalindrome(subString, subStrings)) {
 			  if (subString.length >= 2) {
-					output.push(subString);
+					palinStringAll.push(subString);
 			  }
 		  }
 	  }
   }
-  if (output.length > 0) {
-	  return output;
+  for (let k = 0; k < palinStringAll.length; k++) {
+    if (longestWord < palinStringAll[k].length) {
+      longestWord = palinStringAll[k].length;
+      palinStringLongest = palinStringAll[k]
+    }
+  }
+  if (palinStringAll.length > 0) {
+	  return {"all": palinStringAll, "longest": palinStringLongest};
   } else {
 	  return "This is not a Palindrome String.";
   }
